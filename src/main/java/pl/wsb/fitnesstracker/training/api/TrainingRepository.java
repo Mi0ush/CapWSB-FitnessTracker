23fea2cd97481c5d6bd010b94501d132d929afb5
Mi0ush <milosz070701@gmail.com>
2026-01-18 17:27:00 +0100

    lab6

--- src/main/java/pl/wsb/fitnesstracker/training/api/TrainingRepository.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/TrainingRepository.java
@@ -1,10 +1,15 @@
 package pl.wsb.fitnesstracker.training.api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.util.List;
+import java.util.Date;
 
 /**
  * Repository interface for accessing and managing {@link Training} entities in the database.
  * Extends {@link JpaRepository} to provide standard CRUD operations.
  */
 public interface TrainingRepository extends JpaRepository<Training, Long> {
+    List<Training> findByUser_IdAndEndTimeAfter(Long userId, Date date);
 }
